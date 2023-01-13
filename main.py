4e92d03a79ac223984cfb05bf60df053febd6fd7
sudo <99032942+sudo001@users.noreply.github.com>
2023-01-13 22:18:35 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+def bypass(url):
+    data = {
+        "url": url
+    }
+
+    return requests.post("https://api.bypass.vip/", data=data).json()["destination"]
+
+url = input("Enter Linkvertise URL: ")
+print(bypass(url))
